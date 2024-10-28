d5db2c54ede49a773f19dd6fa454abfea7308783
Simon M <simon.muscas@gmail.com>
2024-10-28 22:51:18 +0100

    made a common file

---
+++ hosts/common.nix
@@ -0,0 +1,38 @@
+# hosts/common.nix
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./../../modules/printing.nix
+    ./../../modules/audio.nix
+    ./../../modules/bluetooth.nix
+    ./../../modules/gc.nix
+    ./../../modules/swap.nix
+    ./../../modules/fonts.nix
+    ./../../modules/environment-variables.nix
+    ./../../modules/gnome.nix
+    ./../../modules/display-manager.nix
+    ./../../modules/energy-management.nix
+    ./../../modules/info-fetchers.nix
+    ./../../modules/hardware.nix
+    ./../../modules/bootloader.nix
+    ./../../modules/networking.nix
+    ./../../modules/programs.nix
+    ./../../modules/user.nix
+    ./../../modules/system.nix
+    ./../../modules/locale.nix
+    ./../../modules/devshells.nix
+  ];
+
+  programs.gamemode.enable = true;
+  programs.steam.enable = true;
+
+  environment.sessionVariables = {
+    FLAKE = "/home/simon/.nixos";
+  };
+
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+}
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -7,28 +7,7 @@
   imports = [
     #     inputs.xremap-flake.nixosModules.default
     ./hardware-configuration.nix
-    ./../../modules/printing.nix
-    ./../../modules/audio.nix
-    ./../../modules/bluetooth.nix
-    ./../../modules/gc.nix
-    ./../../modules/swap.nix
-    ./../../modules/fonts.nix
-    ./../../modules/environment-variables.nix
-    ./../../modules/wayland.nix
-    #./modules/hyprland/default.nix
-    ./../../modules/gnome.nix
-    ./../../modules/display-manager.nix
-    ./../../modules/energy-management.nix
-    ./../../modules/info-fetchers.nix
-    ./../../modules/hardware.nix
-    ./../../modules/bootloader.nix
-    ./../../modules/networking.nix
-    ./../../modules/programs.nix
-    ./../../modules/user.nix
-    ./../../modules/system.nix
-    ./../../modules/locale.nix
-    # ./../../modules/xserver.nix
-    ./../../modules/devshells.nix
+    ./../common.nix
   ];
 
   programs.gamemode.enable = true;
