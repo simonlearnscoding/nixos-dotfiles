f4ec454a225432a4d5f6f5c8c9694769816fbf1a
Simon M <simon.muscas@gmail.com>
2024-11-19 17:01:16 +0100

    add nwg-dock configfile to homemanager

--- home/nwg-dock.nix
+++ home/nwg-dock.nix
@@ -6,4 +6,9 @@
   home.packages = with pkgs; [
     nwg-dock-hyprland
   ];
+
+  home.file.".config/nwg-dock-hyprland/" = {
+    source = builtins.path {path = ./configfiles/nwg-dock-hyprland;};
+    recursive = true;
+  };
 }
