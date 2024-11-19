900134da4c0b6a503802a4dd3b6523102a457054
Simon M <simon.muscas@gmail.com>
2024-11-19 21:08:50 +0100

    made blur a bit prettier

--- home/hyprland/config/blur.nix
+++ home/hyprland/config/blur.nix
@@ -1,15 +1,15 @@
 {
-  # wayland.windowManager.hyprland.settings.general = {
-  #   # gaps_in = 16;
-  #   # gaps_out = 16;
-  # };
   wayland.windowManager.hyprland.settings.decoration = {
-    rounding = 4;
+    rounding = 8; # Slightly increased for smoother, rounded corners
     blur = {
-      noise = 0.01;
-      size = 18;
-      vibrancy = 0.3;
-      popups = true;
+      noise = 0.03; # Adjusted for a more refined blur texture
+      size = 8; # Increased for a smoother and more intense blur effect
+      passes = 3;
+      vibrancy = 0.6; # Slightly enhanced vibrancy for better contrast
+      contrast = 1;
+      popups = false; # Keep blur for popups
+      brightness = 1;
+      vibrancy_darkness = 0.1;
     };
   };
 }
