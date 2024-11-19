94bb134082b7a7c04966344b63799b3b77f84245
Simon M <simon.muscas@gmail.com>
2024-11-19 16:48:40 +0100

    wofi update

--- home/wofi.nix
+++ home/wofi.nix
@@ -1,6 +1,10 @@
-{...}: {
+{ ... }: {
   programs.wofi = {
     enable = true;
+    # Define a custom configuration source for Wofi
+    config = {
+      source = "./configfiles/wofi";
+    };
   };
-  # programs.rofi.theme = "rose-pine";
+
 }
