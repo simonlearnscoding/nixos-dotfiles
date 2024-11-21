28b0edb900a0d95fcf484e865d40eeac599501a0
Simon M <simon.muscas@gmail.com>
2024-11-21 22:59:53 +0100

    move syncthing to home folder

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,34 +1,43 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  systemd.services.syncthing = {
+    serviceConfig = {
+      # Set environment variables to point to the secret file paths
+      Environment = {
+        SYNCTHING_SCHWEIZ_ID = "${config.sops.secrets.SYNCTHING_SCHWEIZ_ID.path}";
+        SYNCTHING_SALZBURG_ID = "${config.sops.secrets.SYNCTHING_SALZBURG_ID.path}";
+      };
+
+      # Debugging step: echo the variables to confirm they are set
+      ExecStartPre = ''
+        if [ -z "$SYNCTHING_SCHWEIZ_ID" ]; then
+          echo "Error: SYNCTHING_SCHWEIZ_ID is not set" >> /var/log/syncthing-debug.log
+        else
+          echo "SYNCTHING_SCHWEIZ_ID=$SYNCTHING_SCHWEIZ_ID" >> /var/log/syncthing-debug.log
+        fi
+
+        if [ -z "$SYNCTHING_SALZBURG_ID" ]; then
+          echo "Error: SYNCTHING_SALZBURG_ID is not set" >> /var/log/syncthing-debug.log
+        else
+          echo "SYNCTHING_SALZBURG_ID=$SYNCTHING_SALZBURG_ID" >> /var/log/syncthing-debug.log
+        fi
+      '';
+    };
+  };
+
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     settings.gui.address = "127.0.0.1:8384";
-
-    # Use extraFlags to pass arguments
     extraFlags = [
       "--no-browser"
       "--gui-address=127.0.0.1:8384"
-      "--config=/var/lib/syncthing/.config/syncthing"
-      "--data=/var/lib/syncthing"
+      "--config=/home/simon/.config/syncthing"
+      "--data=/home/simon/"
     ];
   };
-  # I gave up on trying to set this up with secrets
-  # settings.devices = {
-  #   "schweiz-server" = {
-  #     id = "${config.sops.secrets."SYNCTHING_SCHWEIZ_ID".path}";
-  #     name = "schweiz-server";
-  #   };
-  #
-  #   "salzburg-server" = {
-  #     id = "${config.sops.secrets."SYNCTHING_SCHWEIZ_ID".path}";
-  #     name = "schweiz-server";
-  #   };
-
-  # Inject secrets via environment variables
-
-  # Example: Inject environment variables into Syncthing
-  # systemd.services.syncthing.environment = {
-  #   SYNCTHING_SCHWEIZ_ID = "${config.sops.secrets."SYNCTHING_SCHWEIZ_ID".path}";
-  #   SYNCTHING_SALZBURG_ID = "${config.sops.secrets."SYNCTHING_SALZBURG_ID".path}";
-  # };
 }
