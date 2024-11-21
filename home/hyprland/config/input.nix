a6e5ab1e9f4464931a648ccedba1313737b81933
simonlearnscoding <simon.muscas@gmail.com>
2024-11-21 10:36:40 +0100

    add deluge

--- home/hyprland/config/input.nix
+++ home/hyprland/config/input.nix
@@ -1,11 +1,15 @@
-# hyprland/input.nix
 {
-  kb_layout = "us,de";
-  kb_options = "grp:alt_caps_toggle";
-  numlock_by_default = true;
-  follow_mouse = 1;
-  sensitivity = 1.4;
-  touchpad = {
-    natural_scroll = true;
+  input = {
+    kb_layout = "us,de";
+    kb_options = "grp:alt_caps_toggle";
+    numlock_by_default = true;
+    follow_mouse = 1;
+    sensitivity = 1.4;
+    touchpad = {
+      natural_scroll = true; # Enable natural scroll for touchpad
+    };
+    mouse = {
+      natural_scroll = true; # Enable natural scroll for mouse
+    };
   };
 }
