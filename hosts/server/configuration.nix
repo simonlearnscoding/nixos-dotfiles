03bfd85e997dc61295fbeb63e3d721f9d351219f
Simon M <simon.muscas@gmail.com>
2024-12-23 08:18:55 +0100

    set static ip on server

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -10,6 +10,13 @@
     ./../common.nix
   ];
 
+  networing.interfaces.wlp3s0.ipv4.addresses = [
+    {
+      address = "192.168.1.112";
+      prefixLength = "24";
+    }
+  ];
+
   programs.zsh.enable = true;
   networking.hostName = "simon-server";
   programs.gamemode.enable = true;
