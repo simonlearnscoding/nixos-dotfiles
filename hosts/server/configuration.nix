25015e2fa9cdafd78bdccf45f0a6d486f9b8f4c5
simon <simon.muscas@gmail.com>
2024-12-28 15:11:01 +0100

    setup ip and hware

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -10,9 +10,9 @@
     ./../common.nix
   ];
 
-  networking.interfaces.wlp3s0.ipv4.addresses = [
+  networking.interfaces.enp1s0.ipv4.addresses = [
     {
-      address = "192.168.1.112";
+      address = "192.168.1.23";
       prefixLength = 24;
     }
   ];
