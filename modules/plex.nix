30c378f8fc5e2907c339122675efb58eaef83412
Simon M <simon.muscas@gmail.com>
2025-01-06 23:07:25 +0100

    fix plex

--- modules/plex.nix
+++ modules/plex.nix
@@ -1,7 +1,8 @@
 {...}: {
   services.plex = {
     enable = true;
-    acceptedEula = true; # You must accept the Plex EULA.
-    dataDir = "/var/lib/plex"; # Default directory for Plex data (can be customized).
+    dataDir = "/mnt/drive/movies"; # Default directory for Plex data (can be customized).
+    user = "plex"; # User to run the Plex Media Server as.
+    group = "plex"; # Group to run the Plex Media Server as.
   };
 }
