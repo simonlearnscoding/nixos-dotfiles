7922639d37938f3e4c19d7afa51a64664919903e
Simon M <simon.muscas@gmail.com>
2025-01-07 00:19:32 +0100

    forward x11

--- modules/system.nix
+++ modules/system.nix
@@ -6,7 +6,10 @@
   nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    forwardX11 = true;
+  };
 
   environment.systemPackages = with pkgs; [
     home-manager
