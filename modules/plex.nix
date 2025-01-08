26291824be32cb4c826fc5a0c5e1083c4cbc581f
Simon M <simon.muscas@gmail.com>
2025-01-08 19:47:46 +0100

    plex user now

--- modules/plex.nix
+++ modules/plex.nix
@@ -2,7 +2,7 @@
   services.plex = {
     enable = true;
     dataDir = "/var/lib/plex"; # Default directory for Plex data (can be customized).
-    user = "simon"; # User to run the Plex Media Server as.
-    group = "simon"; # Group to run the Plex Media Server as.
+    user = "plex"; # User to run the Plex Media Server as.
+    group = "plex"; # Group to run the Plex Media Server as.
   };
 }
