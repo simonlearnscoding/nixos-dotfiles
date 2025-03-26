a08dc3bad822fcd270936fbeab10d99cad35c9a0
simon <simon.muscas@gmail.com>
2025-03-26 23:46:37 +0100

    get rid of the python devshells

--- modules/devshells.nix
+++ modules/devshells.nix
@@ -1,59 +1,28 @@
+{config, pkgs, lib }:
+
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  myPython = pkgs.python3.withPackages (pypkgs:
-    with pypkgs; [
-      setproctitle
-      ijson
-      numpy
-      pillow
-      psutil
-      requests
-      setproctitle # This was missing in the explicit list
-      toml
-      watchdog
-      # Add other required packages from the error logs
-      pygobject3
-      dbus-python
-      fabric
-      # You can add other dependencies here if needed
-    ]);
-in {
-  environment.systemPackages = with pkgs;
-    [
-      myPython
-      nodejs_20
-    ]
-    ++ (lib.mapAttrsToList (name: value: pkgs.writeShellScriptBin name value) {
-      python = "${myPython}/bin/python";
-      python3 = "${myPython}/bin/python";
-      # If you have additional wrappers for other tools, list them here.
-      yarn = "corepack yarn \$@";
-      yarnpkg = "corepack yarnpkg \$@";
-      pnpm = "corepack pnpm \$@";
-      pnpx = "corepack pnpx \$@";
-      npm = "corepack npm \$@";
-      npx = "corepack npx \$@";
-    });
 
-  environment.sessionVariables = {
-    # Prepend the custom python's bin to PATH to ensure it's picked up first.
-    PATH = "${myPython}/bin:" + lib.getEnv "PATH";
-    LD_LIBRARY_PATH = lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-    ];
-  };
+environment.systemPackages = (lib.mapAttrsToList (name: value: (pkgs.writeShellScriptBin name value)) {
+    python = "nix-ld-run poetry run -C ~/.config/python/ python $@";
+    python3 = "nix-ld-run poetry run -C ~/.config/python/ python $@";
+    pyinit = "mkdir -p ~/.config/python/ && poetry init -C ~/.config/python/";
+    pi = "nix-ld-run poetry -C ~/.config/python/ add $@";
+    pu = "nix-ld-run poetry -C ~/.config/python/ remove $@";
+    py = "nix-ld-run poetry -C ~/.config/python/ run bpython $@";
+    pyetry = "nix-ld-run poetry -C ~/.config/python/ $@";
 
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      pkgs.stdenv.cc.cc
-      pkgs.zlib
-      pkgs.openssl
-    ];
-  };
-}
+    yarn = "corepack yarn $@";
+    yarnpkg = "corepack yarnpkg $@";
+    pnpm = "corepack pnpm $@";
+    pnpx = "corepack pnpx $@";
+    npm = "corepack npm $@";
+    npx = "corepack npx $@";
+
+    nix-ld-run = ''
+        LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH" LD="$NIX_LD" $@
+    '';
+});
+programs.nix-ld.enable = true;
+programs.nix-ld.libraries = [];
+  # Put libraries here that you need to use for python libraries
+  }
