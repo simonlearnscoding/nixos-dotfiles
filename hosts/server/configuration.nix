62aca886dd0b888f896dafa54c2a381c282503aa
simon <simon.muscas@gmail.com>
2025-03-27 11:31:31 +0100

    remove static ip

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -17,13 +17,14 @@ in {
   environment.systemPackages = with pkgs; [
     xorg.xauth
     firefox
+    cloudflared
   ];
-  networking.interfaces.enp1s0.ipv4.addresses = [
-    {
-      address = "192.168.1.23";
-      prefixLength = 24;
-    }
-  ];
+  # networking.interfaces.enp1s0.ipv4.addresses = [
+  #   {
+  #     address = "192.168.1.23";
+  #     prefixLength = 24;
+  #   }
+  # ];
 
   programs.zsh.enable = true;
   networking.hostName = "simon-server";
