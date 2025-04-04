535ea80b8ff05ed49d197bebcdea10bc7580ff3d
simonlearnscoding <simon.muscas@gmail.com>
2025-04-04 09:43:52 +0200

    nginx

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -6,34 +6,24 @@
   services.nginx = {
     enable = true;
 
-    # Define Syncthing as an upstream service
     upstreams.syncthing.servers = {
       "127.0.0.1:8384" = {};
     };
 
-    virtualHosts."syncthing.yourdomain.com" = {
-      # Only listen on localhost since Cloudflare will proxy to this
+    virtualHosts."syncthing-proxy" = {
       listen = [
         {
           addr = "127.0.0.1";
           port = 3001;
         }
       ];
-      serverName = "syncthing.yourdomain.com";
 
-      locations."/" = {
-        proxyPass = "http://syncthing";
-        proxyWebsockets = true; # For real-time updates
-
+      locations."/syncthing/" = {
         extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-
-          # Cleaner path rewriting using map directive
-          proxy_redirect ~^(https?://[^/]+)(/.+)$ $1/syncthing$2;
+          rewrite ^/syncthing(/.*)$ $1 break;
         '';
+        proxyPass = "http://syncthing";
+        proxyWebsockets = true;
       };
     };
   };
