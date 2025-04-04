b7722d3c4141eeca4461e89f3abc10a7f8233e84
simonlearnscoding <simon.muscas@gmail.com>
2025-04-04 09:32:18 +0200

    syncthing

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -5,51 +5,35 @@
 }: {
   services.nginx = {
     enable = true;
-    virtualHosts."syncthing-proxy" = {
+
+    # Define Syncthing as an upstream service
+    upstreams.syncthing.servers = {
+      "127.0.0.1:8384" = {};
+    };
+
+    virtualHosts."syncthing.yourdomain.com" = {
+      # Only listen on localhost since Cloudflare will proxy to this
       listen = [
         {
           addr = "127.0.0.1";
           port = 3001;
         }
       ];
+      serverName = "syncthing.yourdomain.com";
 
-      locations."/syncthing/" = {
+      locations."/" = {
+        proxyPass = "http://syncthing";
+        proxyWebsockets = true; # For real-time updates
+
         extraConfig = ''
-          # Strip /syncthing prefix when forwarding
-          rewrite ^/syncthing(/.*)$ $1 break;
-          proxy_pass http://127.0.0.1:8384;
-
-          # Essential headers
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
 
-          # Fix ALL paths in responses
-          sub_filter_once off;
-          sub_filter_types *;  # Apply to all content types
-
-          # HTML/JS/CSS path rewrites
-          sub_filter 'href="/' 'href="/syncthing/';
-          sub_filter 'src="/' 'src="/syncthing/';
-          sub_filter 'url(/' 'url(/syncthing/';
-          sub_filter "url('/" "url('/syncthing/";
-          sub_filter 'url("/' 'url("/syncthing/';
-          sub_filter "='/" "='/syncthing/";
-          sub_filter '="/' '="/syncthing/';
-
-          # Fix API/redirect paths
-          sub_filter '"/rest/' '"/syncthing/rest/';
-          sub_filter 'Location: /' 'Location: /syncthing/';
-
-          # WebSocket support
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "upgrade";
+          # Cleaner path rewriting using map directive
+          proxy_redirect ~^(https?://[^/]+)(/.+)$ $1/syncthing$2;
         '';
-      };
-
-      # Handle static assets separately
-      locations."/syncthing/vendor/" = {
-        proxyPass = "http://127.0.0.1:8384/vendor/";
       };
     };
   };
