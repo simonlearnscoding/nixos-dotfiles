929f3943a9f1cbd75bfb300c34cb74c87468af34
simonlearnscoding <simon.muscas@gmail.com>
2025-04-05 13:16:32 +0200

    plex caddy

--- modules/plex.nix
+++ modules/plex.nix
@@ -5,4 +5,12 @@
     user = "plex"; # User to run the Plex Media Server as.
     group = "plex"; # Group to run the Plex Media Server as.
   };
+  services.caddy = {
+    enable = true;
+    virtualHosts."plex.simone-muscas.com" = {
+      extraConfig = ''
+        reverse_proxy localhost:32400
+      '';
+    };
+  };
 }
