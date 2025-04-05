73df5fd7c0a92b27bd3103eff88f2c80326fb135
simon <simon.muscas@gmail.com>
2025-04-05 19:56:53 +0200

    fix the plex stuff

--- modules/plex.nix
+++ modules/plex.nix
@@ -1,25 +1,30 @@
 {...}: {
   services.plex = {
     enable = false;
-    # dataDir = "/var/lib/plex"; # Default directory for Plex data (can be customized).
-    # user = "plex"; # User to run the Plex Media Server as.
-    # group = "plex"; # Group to run the Plex Media Server as.
+    dataDir = "/var/lib/plex"; # Default directory for Plex data (can be customized).
+    user = "plex"; # User to run the Plex Media Server as.
+    group = "plex"; # Group to run the Plex Media Server as.
   };
-  # systemd.services.plex.environment = {
-  #   PLEX_CLAIM = "claim-tykNGBz5fDp_hKv4KSok";
-  # };
   services.caddy = {
     enable = true;
     virtualHosts."plex.simone-muscas.com" = {
       extraConfig = ''
-            reverse_proxy localhost:32400{
-            transport http {
-                tls_insecure_skip_verify
+        # Required for Plex Web
+        reverse_proxy localhost:32400 {
+          transport http {
+            tls_insecure_skip_verify  # Only if you use self-signed certs
+          }
+          header_up Host {host}
+          header_up X-Real-IP {remote}
+          header_up X-Forwarded-For {remote}
+          header_up X-Forwarded-Proto {scheme}
+        }
 
-              }
-              header_up Host {host}
-              header_up X-Real-IP {remote}
-              header_up X-Forwarded-Proto {scheme}
+        # WebSocket support (critical for Plex)
+        reverse_proxy /socket localhost:32400 {
+          transport http {
+            tls_insecure_skip_verify
+          }
         }
       '';
     };
