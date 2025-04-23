b01b187910a17259ebfdd3ffed20b43e483ad922
simonlearnscoding <simon.muscas@gmail.com>
2025-04-23 13:17:25 +0200

    Waydroid fix

--- modules/waydroid.nix
+++ modules/waydroid.nix
@@ -9,6 +9,6 @@
   # Optional: Add Android udev rules (for ADB debugging)
   services.udev.packages = with pkgs; [android-udev-rules];
 
-  # Optional: Use isolated network mode (default is "bridge")
-  virtualisation.waydroid.network = "isolated"; # or "bridge" (default)
+  # # Optional: Use isolated network mode (default is "bridge")
+  # virtualisation.waydroid.network = "isolated"; # or "bridge" (default)
 }
