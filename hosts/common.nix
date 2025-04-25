72be5f0327d560b3c8881a53ddaa3d5152d014ec
Simon M <simon.muscas@gmail.com>
2025-04-25 21:30:07 +0200

    add syncthing

--- hosts/common.nix
+++ hosts/common.nix
@@ -8,6 +8,8 @@
   imports = [
     ./../modules/user.nix
     ./../modules/docker.nix
+
+    ./../modules/syncthing.nix
     # ./../modules/sops.nix
     ./../modules/gc.nix
     ./../modules/swap.nix
