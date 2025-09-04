b63f36e9418a89cc9042c8284d9510b6c367f5dc
Simon M <simon.muscas@gmail.com>
2025-09-04 13:37:56 +0200

    stylix catppuccin

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,13 +1,13 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   stylix.enable = true;
   stylix.autoEnable = true;
+
   stylix.image = ../files/wallpapers/wallpaper-2.JPG;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+
+  # Pick a Catppuccin variant from base16-schemes
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+
+  stylix.targets.plymouth.enable = false; # avoid conflicts
   stylix.targets.grub.enable = true;
   stylix.targets.gtk.enable = true;
-  # stylix.targets.spicetify.enable = true;
 }
