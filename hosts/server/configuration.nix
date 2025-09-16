80b8de8aff4247c7d9ed9b09094b59fcf3f302b3
Simon M <simon.muscas@gmail.com>
2025-09-16 10:59:07 +0200

    fix server?

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -7,39 +7,41 @@
   UUID = "04c67b4a-ead1-4613-9abc-2985e9202e5c";
 in {
   imports = [
-    #     inputs.xremap-flake.nixosModules.default
     ./hardware-configuration.nix
     ./../common.nix
-    ./../../modules/ci-user.nix
     ./../serverstuff.nix
   ];
 
   environment.systemPackages = with pkgs; [
     xorg.xauth
-    firefox # in case I ever need to login via browser
+    firefox
     cloudflared
   ];
-  # networking.interfaces.enp1s0.ipv4.addresses = [
-  #   {
-  #     address = "192.168.1.23";
-  #     prefixLength = 24;
-  #   }
-  # ];
 
   hardware.enableRedistributableFirmware = true;
   boot.extraModulePackages = with pkgs.linuxPackages; [
-    rtl88xxau-aircrack # common Realtek driver
+    rtl88xxau-aircrack
   ];
 
   programs.zsh.enable = true;
   networking.hostName = "simon-server";
-  programs.gamemode.enable = true;
 
-  services.fstrim.enable = true; # runs weekly via timer
+  services.fstrim.enable = true;
+
   fileSystems."/mnt/drive" = {
     device = "/dev/disk/by-uuid/${UUID}";
     fsType = "ext4";
-    options = ["nofail" "x-systemd.device-timeout=1s" "x-systemd.automount"];
+    options = [
+      "nofail"
+      "x-systemd.device-timeout=1s"
+      "x-systemd.automount"
+    ];
   };
+
+  # Mount the moment the disk appears
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="block", ENV{ID_FS_UUID}=="${UUID}", ENV{SYSTEMD_WANTS}+="mnt-drive.mount"
+  '';
+
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 }
